0137d4202d7971c3030a3558c980622b016c4c29
barischj <barischj@tcd.ie>
2017-01-15 19:51:57 +0000

    finished static analysis

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,19 @@
 module Main where
 
-import Lib
+import Lib (runInterpreter)
+import Static (analyse, printErrs)
 import System.Environment
 
+-- | Runs the interpreter on a file.
+-- If the file doesn't parse or has static analysis errors then the interpreter
+-- will NOT be run.
 main :: IO ()
 main = do
-  [filepath] <- getArgs
-  file <- readFile filepath
-  runInterpreter $ read file
+    [filepath] <- getArgs
+    file <- readFile filepath
+    let stmt = read file
+    errs <- analyse stmt
+    case errs of
+        [] -> runInterpreter stmt
+        _  -> printErrs errs
+  
--- src/Static.hs
+++ src/Static.hs
@@ -1,4 +1,4 @@
-module Static where
+module Static (analyse, printErrs) where
 
 import           Control.Monad.Identity
 import           Control.Monad.State
@@ -10,44 +10,72 @@ import           Lib
 data StaticErr = Uninit Name | Unused Name
 
 -- Track the initialised variables.
-type StaticState = Set.Set Name
+data StaticState = StaticState
+    { sInitVars :: Set.Set Name, sAccessVars :: Set.Set Name }
+
+-- Empty state tracking no variables.
+emptyState :: StaticState
+emptyState = StaticState Set.empty Set.empty
 
 -- Log any errors and maintain state.
-type Static a = WriterT [StaticErr] (StateT StaticState Identity) a
+type Static a = WriterT [StaticErr] (StateT StaticState IO) a
+
+-- Run the Static monad.
+runStatic :: Static a -> IO ((a, [StaticErr]), StaticState)
+runStatic static = runStateT (runWriterT static) emptyState 
+
+-- Find static analysis errors.
+analyse :: Statement -> IO [StaticErr]
+analyse stmt = do
+    ((_, errs), StaticState initialised accessed) <- runStatic $ staticS stmt
+    -- Unused variables are those initialised but unused.
+    let unused = Set.toList $ initialised `Set.difference` accessed
+    return $ errs ++ map Unused unused
 
 -- Add a variable as initialised.
-initVar :: Name -> Static ()
-initVar name = modify $ Set.union $ Set.singleton name
+addInitVar :: Name -> Static ()
+addInitVar name = modify (\s ->
+    s { sInitVars = sInitVars s `Set.union` Set.singleton name})
+
+-- Add a variable as accessed.
+addAccessVar :: Name -> Static ()
+addAccessVar name = modify (\s ->
+    s { sAccessVars = sAccessVars s `Set.union` Set.singleton name })
+
+-- | Prints any static analyis errors.
+printErrs :: [StaticErr] -> IO ()
+printErrs errs = putStrLn "Static analysis errors:" >> mapM_ printErr errs
+
+printErr :: StaticErr -> IO ()
+printErr (Uninit name) = putStrLn $ "Access of uninitialised variable " ++ name
+printErr (Unused name) = putStrLn $ "Initialised yet unused variable " ++ name
 
 -- Static analysis of expressions ---------------------------------------------
-
--- data Expr =
---       Const Val
---     | Add Expr Expr
---     | Sub Expr Expr
---     | Mul Expr Expr
---     | Div Expr Expr
---     | And Expr Expr
---     | Or  Expr Expr
---     | Not Expr
---     | Eq  Expr Expr
---     | Gt  Expr Expr
---     | Lt  Expr Expr
---     | Var Name
---     deriving (Eq, Read, Show)
 
 staticE :: Expr -> Static ()
 staticE (Const _)   = return ()
 staticE (Add e1 e2) = staticE e1 >> staticE e2
+staticE (Sub e1 e2) = staticE e1 >> staticE e2
+staticE (Mul e1 e2) = staticE e1 >> staticE e2
+staticE (Div e1 e2) = staticE e1 >> staticE e2
+staticE (And e1 e2) = staticE e1 >> staticE e2
+staticE (Or  e1 e2) = staticE e1 >> staticE e2
+staticE (Not e1)    = staticE e1
+staticE (Eq  e1 e2) = staticE e1 >> staticE e2
+staticE (Gt  e1 e2) = staticE e1 >> staticE e2
+staticE (Lt  e1 e2) = staticE e1 >> staticE e2
 staticE (Var name)  = do
-    initialised <- get
+    initialised <- fmap sInitVars get
     unless (name `Set.member` initialised) $ tell [Uninit name]
+    addAccessVar name
 
 -- Static analysis of statements ----------------------------------------------
 
 staticS :: Statement -> Static ()
-
-staticS (Assign name expr) = do
-  staticE expr
-  initVar name
-
+staticS (Assign name e1) = staticE e1 >> addInitVar name
+staticS (If e1 s1 s2)    = staticE e1 >> staticS s1 >> staticS s2
+staticS (While e1 s1)    = staticE e1 >> staticS s1
+staticS (Print e1)       = staticE e1
+staticS (Seq s1 s2)      = staticS s1 >> staticS s2
+staticS (Try s1 s2)      = staticS s1 >> staticS s2
+staticS Pass             = return ()
